f406acec4fd53fff8d1fd5ac27102ce2216bf14a
Vishnu N <vishnunarasim20@gmail.com>
2022-03-05 12:48:39 +0530

    JS_Assignment_4

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,9 @@
 function CreateObject(arr) {
-    // Write your code here
+    const object = {};
+    for(let i = 0 ; i<arr.length;i+=2){
+        object[arr[i]] = arr[i+1];
+    }
+    return object
 }
 
 module.exports = CreateObject;
